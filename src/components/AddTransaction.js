df8c99255405af01fc040a91d66f258f308002ff
robertmoore40 <robertcmoore92@gmail.com>
2020-12-22 14:43:33 -0500

    export const

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,2 +1,5 @@
 import React, {useState, useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+    const [text, setText] = useState('');
