c25f91b2571ef4f834ca1b644d13a33ac2963add
robertmoore40 <robertcmoore92@gmail.com>
2020-12-23 07:40:40 -0500

    added form ending

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -17,19 +17,18 @@ export const AddTransaction = () => {
     
     addTransaction(newTransaction);
 }
-
 return (
     <>
       <h3>Add new transaction</h3>
-      <form onSubmit={onSubmit}></form>
-      <div className="form-control">
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
             >Amount <br />
-              (negative - expense, positive - income)</label
+            (negative - expense, positive - income)</label
           >
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
