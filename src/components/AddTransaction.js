15b9d3fe7fbb2877be23cf8edc04d3ddfa5f3840
robertmoore40 <robertcmoore92@gmail.com>
2020-12-23 07:39:11 -0500

    added placeholder value

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -22,3 +22,7 @@ return (
     <>
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}></form>
+      <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        </div>
