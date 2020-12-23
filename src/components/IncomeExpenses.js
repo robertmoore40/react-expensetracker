8aa33a6bcf99c3c076dda1b89827081b7564d163
robertmoore40 <robertcmoore92@gmail.com>
2020-12-23 07:46:59 -0500

    added income container

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -16,3 +16,7 @@ export const IncomeExpenses = () => {
     amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
     -1
   ).toFixed(2);
+
+  return (
+    <div className="inc-exp-container">
+        <div>
