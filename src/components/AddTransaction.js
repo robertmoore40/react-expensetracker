4775fa9460f6be963aa50a73f9ba3c25fa450ddf
robertmoore40 <robertcmoore92@gmail.com>
2020-12-23 07:40:17 -0500

    finished return

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -33,3 +33,8 @@ return (
           >
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  )
+}
