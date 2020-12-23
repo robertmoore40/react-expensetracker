d208206283a8e5a744b2535d1dcf8010ac3c861c
robertmoore40 <robertcmoore92@gmail.com>
2020-12-23 07:39:28 -0500

    added form control

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -26,3 +26,6 @@ return (
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
         </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >Amount <br />
