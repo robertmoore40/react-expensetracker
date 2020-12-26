dbc038fa914eb6e295346172531d40e65ecf1841
robertmoore40 <robertcmoore92@gmail.com>
2020-12-26 14:34:51 -0500

    added map

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -10,3 +10,6 @@ export const TransactionList = () => {
     return (
         <>
           <h3>History</h3>
+          <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
