45169ef255bf59cae38e72a2236e02030807d07b
robertmoore40 <robertcmoore92@gmail.com>
2020-12-27 17:59:39 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -5,3 +5,6 @@ import { Balance } from './components/Balance';
 
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
