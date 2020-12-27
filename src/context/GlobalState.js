654548c24cb7557440b4b6e79b0127123012614e
robertmoore40 <robertcmoore92@gmail.com>
2020-12-27 18:07:50 -0500

    added delete transaction function

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -11,3 +11,9 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
   
+    function deleteTransaction(id) {
+        dispatch({
+          type: 'DELETE_TRANSACTION',
+          payload: id
+        });
+      }
