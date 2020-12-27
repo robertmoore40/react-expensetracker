400426105037c2d0306dfee8a260482c16f4b2e1
robertmoore40 <robertcmoore92@gmail.com>
2020-12-27 18:00:50 -0500

    added app and global provider

--- src/App.js
+++ src/App.js
@@ -6,3 +6,7 @@ import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 import { GlobalProvider } from './context/GlobalState';
 import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider></GlobalProvider>
