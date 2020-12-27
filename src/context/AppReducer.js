064ec58467021ac0b096c793b25c2841470c1d1b
robertmoore40 <robertcmoore92@gmail.com>
2020-12-27 18:05:15 -0500

    added state

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,3 +1,7 @@
 export default (state, action) => {
     switch(action.type) {
       case 'DELETE_TRANSACTION':
+        return {
+            ...state,
+            transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+          }
