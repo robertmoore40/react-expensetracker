6737059981f5209b82c46f4019cb757287e23802
robertmoore40 <robertcmoore92@gmail.com>
2020-12-27 17:59:21 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -2,3 +2,6 @@ import React from 'react';
 
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
